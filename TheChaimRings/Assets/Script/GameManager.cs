e01f5b6aa9b47ba04d46d9d948cdbbebc81e189e
DESKTOP-GGBBF12\moris <morishi0318@outlook.jp>
2024-06-23 19:46:56 +0900

    ブランチ切りますー

--- TheChaimRings/Assets/Script/GameManager.cs
+++ TheChaimRings/Assets/Script/GameManager.cs
@@ -1,11 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     void Start()
     {
         EventManager.Instance.playerEvent.AddListener(OnPlayerEvent);
     }
+
 
     void OnPlayerEvent(PlayerEventParams eventParams)
     {
--- TheChaimRings/Assets/Script/Player.cs
+++ TheChaimRings/Assets/Script/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [Header("Settings")] // ここに見出しを追加
     public GameObject pathPrefab; // ステージのプレハブ
     private Rigidbody rb; // プレイヤーのリジッドボディ
     private Vector3 velocityDirection; // プレイヤーの速度ベクトル
@@ -40,7 +42,7 @@ public class Player : MonoBehaviour
     void CreatePath(Transform gateParentTransform)
     {
         // 親オブジェクトのZ座標に基づいてプレハブを生成する位置を決定
-        Vector3 spawnPosition = gateParentTransform.position + new Vector3(0, 0, 20);
+        Vector3 spawnPosition = gateParentTransform.position + new Vector3(0, 0, 22);
 
         // プレイヤーの進行方向に基づいてプレハブを生成
         Instantiate(pathPrefab, spawnPosition, Quaternion.identity);
